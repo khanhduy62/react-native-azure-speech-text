ee39262f5837e8b06f5db589e43d16b2f927fad1
phithu <phithu.dev@gmail.com>
2020-09-15 11:09:58 +0700

    Set default voice name

--- android/src/main/java/br/com/atsneves/ReturnSpeak.java
+++ android/src/main/java/br/com/atsneves/ReturnSpeak.java
@@ -2,11 +2,8 @@ package br.com.atsneves;
 
 public class ReturnSpeak {
 
-
-
     private boolean success;
     private String errorMessage;
-
 
     public ReturnSpeak(boolean success, String errorMessage) {
         this.success = success;
